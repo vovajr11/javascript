c211a256e2bb8094b89f62893b1cdb6145082704
VOVA-PC\Vova <vova.moskalyuk11jr@gmail.com>
2020-11-18 15:01:44 +0200

    add module-3

---
+++ module-3/hw/04-task.js
@@ -0,0 +1,34 @@
+// Напиши функцию countTotalSalary(employees) принимающую объект зарплат.
+// Функция считает общую сумму запрплаты работников и возращает ее.
+// Каждое поле объекта, передаваемого в функцию, имеет вид "имя":"зарплата".
+
+const countTotalSalary = function (employees) {
+  let total = 0;
+  for (const key in employees) {
+    // console.log('key', employees[key]);
+    total += employees[key];
+  }
+
+  return total;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
