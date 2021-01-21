98e4273bfaeab2bfaadfcfb3dadc1e07dc7df8a4
VOVA-PC\Vova <vova.moskalyuk11jr@gmail.com>
2021-01-21 11:23:11 +0200

    bag

--- module-9-hw/src/index.js
+++ module-9-hw/src/index.js
@@ -2,7 +2,7 @@ import menuElement from "./js/menu.json";
 import menuTemplate from "./templetes/templates.hbs";
 import "./js/changeTheme.js";
 import "./css/style.css";
-console.log("hello");
+console.log("hello dsad");
 
 let menu = document.querySelector(".js-menu");
 const menuMarkup = createMenuCardsMarkup(menuElement);
